1e4681c0cefb20b1def182444fd341183fdec06a
Ben OSU <bigbeno37@gmail.com>
2014-12-28 08:01:50 +0000

    Added bootstrap to concept design

---
+++ Concepts/js/main.js
@@ -0,0 +1,6 @@
+/* ==========================================================================
+   Author's custom scripts
+   ========================================================================== */
+$(document).ready(function(){
+    $(".jumbotron").backstretch(["http://fc01.deviantart.net/fs20/i/2007/245/0/a/The_Drill_by_tarrzan.jpg"], {duration:2000, fade:"slow"});
+});
